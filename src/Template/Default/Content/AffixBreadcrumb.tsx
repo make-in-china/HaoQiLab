3d5c985c8d2ceffe483c07ca97ce18ab624e5b8c
灰烬轮回 <692425987@qq.com>
2017-12-28 12:35:20 +0800

    修改样式

--- src/Template/Default/Content/AffixBreadcrumb.tsx
+++ src/Template/Default/Content/AffixBreadcrumb.tsx
@@ -1,6 +1,6 @@
 import React from 'react-ex';
-import { observer, } from 'mobx-react';
-import { observable } from 'mobx';
+// import { observer, } from 'mobx-react';
+// import { observable } from 'mobx';
 import Antd from 'antd-min';
 import BreadcrumbItems from './BreadcrumbItems';
 import Clock from '../../../Components/Clock';
@@ -11,36 +11,38 @@ const logo = require('../../assert/logo.png');
         '.Affix .Breadcrumb .BreadcrumbItem,.Affix .Breadcrumb .BreadcrumbItem a': React.calcStyle({ transition: 'color .3s' })
     }],
     box1: [{
-        '.Affix .logo': [`background-image:url(${logo});background-size:100%;`, 'mdip-5 wem-3 hem-2 float'.split(' ')],
+        '.Affix .logo': [`background-image:url(${logo});background-size:100%;transform:rotate(180deg);`, 'wem-4 hem-4 float'.split(' ')],
         '.Affix': 'padding-left:25px;',
         '.Affix .Breadcrumb': 'float:left;margin:8px 0;',
-        '.Affix .ant-affix': 'left:0 !important;background-color:rgba(0,0,0,0.75);border-bottom-right-radius:1em;',
+        '.Affix .ant-affix': 'left:0 !important;height:2.5em;background-color:rgba(0,0,0,0.75);border-bottom-right-radius:1em;',
         '.Affix,.Affix .Breadcrumb, .ant-breadcrumb-separator,.Affix .Breadcrumb .BreadcrumbItem,.Affix .Breadcrumb .BreadcrumbItem a': 'color:#fff;'
     }],
     box2: [{
         ' .logo': ['uof'.split(' ')],
         '.Affix': 'padding-left:50px;',
         '.Affix .Breadcrumb': 'float:left;margin:16px 0;',
-        '.Affix>div': 'padding-right:4em;'
+        '.Affix>div': 'padding-right:5em;'
     }],
     affix: ['uof zidx-999 inline meml-7'.split(' ')]
 })
-@observer
 export default class AffixBreadcrumb
     extends React.Component<
     {
         page: IPage
     }
     > {
-    @observable affixed = false;
 
+    eclassControl = new React.AsyncEClass('box', 'box2');
+    eclassControl2 = new React.AsyncEClass('mdiplr-50', 'mdiptb-16');
     onChange = (affixed: boolean) => {
-        this.affixed = affixed;
+        this.eclassControl.setClass('box' + (affixed ? '1' : '2'));
+        this.eclassControl2.setClass('mdiptb-' + (affixed ? '8' : '16'));
     }
+
     render() {
         return [
             (
-                <div EClass={'box box' + (this.affixed ? '1' : '2')} className="Affix" >
+                <div EClass={this.eclassControl} className="Affix" >
                     <Antd.Affix
                         EClass="affix"
                         onChange={this.onChange/* 切换背景颜色左右边距 */}
@@ -52,7 +54,7 @@ export default class AffixBreadcrumb
                     </Antd.Affix>
                 </div>
             ), (
-                <div style={{ float: 'right', margin: this.affixed ? '8px 50px' : '16px 50px' }}><Clock /></div>
+                <div EClass={this.eclassControl2} style={{ float: 'right' }}><Clock /></div>
             )
         ];
     }
