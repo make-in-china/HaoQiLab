9d2677d8c6b771280c1e30fb8a79dce0489053f7
灰烬轮回 <692425987@qq.com>
2018-01-04 09:43:38 +0800

    样式自提供

--- src/Page/Play/Component/SkinBoxSetupItem.tsx
+++ src/Page/Play/Component/SkinBoxSetupItem.tsx
@@ -1,17 +1,27 @@
 import React from 'react-ex';
 import Antd from 'antd-more';
 import { observer, observable } from 'mobx-index';
+@React.eclass({
+    line: [
+        'nowrap mdipt-5'.split(' '),
+        {
+            ' .&name': ['text-align:right;', 'mdipr-10 wem-5 inline'.split(' ')],
+            '>input': ['wem-4 inline'.split(' ')]// 待抽离
+        }
+    ],
+})
 @observer
 export default class SkinBoxSetupItem
     extends React.Component<
     {
         source: Record<string, boolean>
+        keyName: string
         title: string
         onChangeCheck: () => void
     }> {
     @observable private renderRandom = Math.random();
     onChange: Antd.Checkbox['props']['onChange'] = (e) => {
-        this.props.source[this.props.title] = e.target.checked;
+        this.props.source[this.props.keyName] = e.target.checked;
         this.props.onChangeCheck();
         this.renderRandom = Math.random();
     }
@@ -19,8 +29,12 @@ export default class SkinBoxSetupItem
         // tslint:disable-next-line:no-unused-expression
         void this.renderRandom;
         return (
-            <div>
-                <span><Antd.Checkbox onChange={this.onChange} checked={this.props.source[this.props.title]}>{this.props.title}</Antd.Checkbox></span>
+            <div EClass="line">
+                <Antd.Tooltip placement="left" title={this.props.keyName}>
+                    <Antd.Checkbox onChange={this.onChange} checked={this.props.source[this.props.keyName]}>
+                        <span EClass="name" >{this.props.title} : </span>
+                    </Antd.Checkbox>
+                </Antd.Tooltip>
                 {this.props.children}
             </div>
         );
