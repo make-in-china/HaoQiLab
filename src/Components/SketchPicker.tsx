a13ee3e05828eb4a6ded986db3838aefddd3d06f
灰烬轮回 <692425987@qq.com>
2018-01-09 17:27:06 +0800

    抽取ModelWindow

--- src/Components/SketchPicker.tsx
+++ src/Components/SketchPicker.tsx
@@ -1,12 +1,11 @@
 import React from 'react-ex';
 import ColorBooth from 'src/Components/ColorBooth';
 import { MouseEventHandler } from 'react';
-import * as ReactDOM from 'react-dom';
 import SketchPickerPanel from 'src/Components/SketchPickerPanel';
-import { RGBA, cssClassNS } from 'src/CSS/CSSClass';
+import { RGBA } from 'src/CSS/CSSClass';
 import { observable, observer } from 'src/Lib/mobx.index';
 import { Ref } from 'react';
-import { getOffset } from 'src/Lib/getPos';
+import { ModelPanel } from 'src/Lib/ModelPanel';
 @React.eclass(/*local eclass rule */{
     colorbox: ['pdip-5 ladip lhem-0 rdip-10 inline bd-12-gray'.split(' ')],
     color: ['wem-3 hem-1 rdip-5'.split(' ')],
@@ -24,6 +23,7 @@ export default class SketchPicker
 
     // #region public property
     @observable color: RGBA = this.props.color;
+    setupModel: ModelPanel | null = null;
     root: HTMLDivElement;
     divBox: HTMLDivElement;
     div: HTMLDivElement;
@@ -33,37 +33,18 @@ export default class SketchPicker
             this.props.onChange(clr);
         }
     }
-
     onClick: MouseEventHandler<HTMLDivElement> = (e) => {
-        const divBox = this.divBox;
-        const div = this.div;
-        const pos = getOffset(this.root);
-        let left = pos.x;
-        if (left < 0) {
-            left = 0;
+        if (this.setupModel) {
+            this.setupModel.open();
         }
-        document.body.appendChild(divBox);
-
-        ReactDOM.render((
-            <SketchPickerPanel color={this.color} onChange={this.onChange} />
-        ), div);
-        
-        divBox.style.left = left + 'px';
-        let top = pos.y - this.divBox.scrollHeight;
-
-        divBox.style.top = top + 'px';
-        const pos2 = getOffset(div);
-        if (pos2.y < 5) {
-            divBox.style.top = top - pos2.y + 5 + 'px';
-        }
-        document.body.addEventListener('click', this.closePanel);
     }
-    onRef: Ref<HTMLDivElement> = (elem) => {
-        if (elem === null) {
+    onRef: Ref<HTMLDivElement> = (instance) => {
+        if (instance === null) {
             return;
         }
-        this.root = elem as HTMLDivElement;
-
+        if (instance) {
+            this.setupModel = new ModelPanel((<SketchPickerPanel color={this.color} onChange={this.onChange} />), instance, 'zidx-9999 pdip-10', true);
+        }
     }
     // #endregion
 
@@ -75,19 +56,6 @@ export default class SketchPicker
 
     constructor(props: SketchPicker['props']) {
         super(props);
-        const eclass = cssClassNS.CSSClass.instance;
-        const divBox = this.divBox = document.createElement('div');
-        eclass.registerClass('abs');
-        eclass.registerClass('pdip-5');
-        eclass.registerClass('zidx-8888');
-        divBox.className = 'abs pdip-5 zidx-8888';
-        divBox.addEventListener('click', function (e: Event) {
-            e.stopPropagation();
-            e.preventDefault();
-        });
-        const div = this.div = document.createElement('div');
-        div.style.marginTop = '-100%';
-        divBox.appendChild(div);
     }
     componentWillMount() {
         this.init(this.props);
@@ -111,9 +79,6 @@ export default class SketchPicker
     // #endregion
 
     // #region private methods
-    closePanel = () => {
-        document.body.removeEventListener('click', this.closePanel);
-        this.divBox.remove();
-    }
+
     // #endregion
 }
