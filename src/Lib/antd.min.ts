1962723b92baf83f3cda38cb4fd677ec9cce5ad4
灰烬轮回 <692425987@qq.com>
2018-01-09 17:30:29 +0800

    从Ex转移一些组件到Base

--- src/Lib/antd.min.ts
+++ src/Lib/antd.min.ts
@@ -1,16 +1,21 @@
 
-import * as Antd from 'antd';
-
+import * as _Antd from 'antd';
+export const Antd = _Antd;
 export default {
-    Affix : Antd.Affix,
-    BackTop : Antd.BackTop,
-    Breadcrumb : Antd.Breadcrumb,
-    Dropdown : Antd.Dropdown,
-    Icon : Antd.Icon,
-    Layout : Antd.Layout,
-    Menu : Antd.Menu,
-    Spin : Antd.Spin,
-    Tabs : Antd.Tabs
+    Affix: _Antd.Affix,
+    BackTop: _Antd.BackTop,
+    Breadcrumb: _Antd.Breadcrumb,
+    Dropdown: _Antd.Dropdown,
+    Icon: _Antd.Icon,
+    Layout: _Antd.Layout,
+    Menu: _Antd.Menu,
+    Spin: _Antd.Spin,
+    Tabs: _Antd.Tabs,
+    Button: _Antd.Button,
+    Tooltip: _Antd.Tooltip,
+    Checkbox: _Antd.Checkbox,
+    InputNumber: _Antd.InputNumber,
+    Switch: _Antd.Switch
 };
 if (Antd === undefined) {
     alert('未导入任何Antd组件');
