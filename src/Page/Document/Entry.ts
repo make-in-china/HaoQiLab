a11745ce5463a8c3a9951c6fdd77727df78dd833
灰烬轮回 <692425987@qq.com>
2018-01-15 18:00:20 +0800

    额，再来一次构建，复制黏贴把key给碰到了

--- src/Page/Document/Entry.ts
+++ src/Page/Document/Entry.ts
@@ -1,6 +1,6 @@
 import AddEntryComponent from 'src/Page/EntryComponent';
 import { IReactComponent } from 'mobx-react';
-export const Key = 'ReactEx';
+export const Key = 'Document';
 export default function entry<T extends IReactComponent>(name: string) {
     return function (ctor: T) {
         AddEntryComponent(ctor.name, Key, ctor);
