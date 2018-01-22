5872dcd50fc4be5787ca218ec148c5e730639890
灰烬轮回 <692425987@qq.com>
2018-01-22 09:00:58 +0800

    添加文字

--- src/Page/Document/Content/Layout.tsx
+++ src/Page/Document/Content/Layout.tsx
@@ -8,14 +8,14 @@ export default class Layout extends React.Component {
         return [
             '后面的不仿了，演示下动态加载',
             (
-            <Antd.Layout>
-                <Sider>Sider</Sider>
                 <Antd.Layout>
-                    <Header>Header</Header>
-                    <Content>Content</Content>
-                    <Footer>Footer</Footer>
+                    <Sider>Sider</Sider>
+                    <Antd.Layout>
+                        <Header>Header</Header>
+                        <Content>Content</Content>
+                        <Footer>Footer</Footer>
+                    </Antd.Layout>
                 </Antd.Layout>
-            </Antd.Layout>
-        )];
+            )];
     }
 }
