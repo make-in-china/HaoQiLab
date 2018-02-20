bee6f1632a7c9c662e9e57616df07267222e41cd
灰烬轮回 <692425987@qq.com>
2018-02-20 20:12:05 +0800

    例子

--- src/Page/Typescript/Content.tsx
+++ src/Page/Typescript/Content.tsx
@@ -30,7 +30,7 @@ export default class App extends React.Component {
                     </ul> */}
                 </p>
                 <p><h3>2.如何给已存在的类加属性和方法？</h3></p>
-                <p  EClass="pdip-chd-15 lbpx-chd-2-dashed">
+                <p EClass="pdip-chd-15 lbpx-chd-2-dashed">
                     如图
                     <div>第一步.
                         <img src={pic2A} alt="" />
@@ -47,3 +47,37 @@ export default class App extends React.Component {
         );
     }
 }
+@addNewPropertyToA({
+    newProperty: 888
+})
+class A {
+    onlyOneProperty: string = '2';
+    // tslint:disable-next-line:no-empty
+    onlyOneMethods(): void { }
+}
+// 利用合并编写新方法声明
+interface A {
+    newMethods(): void;
+}
+// 在厡型链上补完新方法实体
+// tslint:disable-next-line:no-empty
+A.prototype.newMethods = (function () { });
+// 利用合并编写新属性声明
+interface A {
+    newProperty: number;
+}
+// 编写装饰器
+function addNewPropertyToA(initTemplate: Partial<A>) {
+    return function (ctor: typeof A) {
+        return function (this: typeof A) {
+            ctor.apply(this, arguments);
+            // 对新属性赋值
+            for (const key in initTemplate) {
+                this[key] = initTemplate[key];
+            }
+            return this;
+        } as any as typeof A;
+    };
+}
+// 这时候识别出两个方法两个属性
+(new A).
